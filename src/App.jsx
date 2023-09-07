ce1aa820c47da5e2ba7b4a3da0c612e81c5f10fb
2101dudu <eduardo21faria@gmail.com>
2023-09-07 20:10:18 +0100

    Finally added the scrollTo functions on the navbar buttons, allowing for a better experience when navigating the page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from 'react';
+import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import AboutMe from './components/AboutMe';
@@ -9,25 +7,57 @@ import Projects from './components/Projects';
 import ContactMe from './components/ContactMe';
 import Footer from './components/Footer';
 
-
-
 function App() {
+  useEffect(() => {
+    const backToTopButton = document.getElementById("backToTopButton");
+    const backToAboutMeButton = document.getElementById("backToAboutMeButton");
+    const backToProjectsButton = document.getElementById("backToProjectsButton");
+    const backToContactMeButton = document.getElementById("backToContactMeButton");
+
+    const backToTop = () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+    const backToAboutMe = () => {
+      window.scrollTo({ top: 800, behavior: "smooth" });
+    };
+    const backToProjects = () => {
+      window.scrollTo({ top: 1750, behavior: "smooth" });
+    };
+    const backToContactMe = () => {
+      window.scrollTo({ top: 2750, behavior: "smooth" });
+    };
+
+    if (backToTopButton) {
+      backToTopButton.addEventListener("click", backToTop);
+    }
+    if (backToAboutMeButton) {
+      backToAboutMeButton.addEventListener("click", backToAboutMe);
+    }
+    if (backToProjectsButton) {
+      backToProjectsButton.addEventListener("click", backToProjects);
+    }
+    if (backToContactMeButton) {
+      backToContactMeButton.addEventListener("click", backToContactMe);
+    }
+  }, []);
 
   return (
     <>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
-        <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;500&family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet"></link>
-      </head>
-      <Navbar></Navbar>
-      <Hero></Hero>
-      <AboutMe></AboutMe>
-      <Projects></Projects>
-      <ContactMe></ContactMe>
-      <Footer></Footer>
+      <div className="scroll-smooth">
+        <header>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+          <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;500&family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet" />
+        </header>
+        <Navbar />
+        <Hero />
+        <AboutMe />
+        <Projects />
+        <ContactMe />
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,35 +2,36 @@ import React from 'react';
 
 
 
+
+
 function Navbar() {
-    return (
-        <nav class="invisible md:visible bg-tempwhite fixed w-full z-20 top-0 left-0 border-b border-gray-200">
+  return (
+    <nav class="invisible md:visible bg-tempwhite fixed w-full z-20 top-0 left-0 border-b border-gray-200">
           <div class="max-w-screen-xl flex justify-between mx-auto md:pl-12 lg:pl-4">
-            <a href="" class="flex items-center">
+            <button id="backToTopButton" class="flex items-center">
                 <span class="self-center text-4xl font-black font-lato whitespace-nowrap text-tempblack dark:text-tempwhite">Eduardo Faria</span>
-            </a>
+            </button>
             <div class="items-center justify-end hidden w-full md:flex md:max-w-md lg:max-w-xl md:order-1 md:pr-12 lg:pr-4" id="navbar-sticky">
               <ul class="flex flex-row md:my-2 lg:my-4 space-x-5 lg:space-x-10">
                 <li class="pt-2 pr-1">
-                  <a href="#" class="w-full text-right md:duration-300 md:hover:scale-105 md:text-l lg:text-xl block pt-1 font-lato font-bold text-tempblack">About me</a>
+                  <button id="backToAboutMeButton" class="w-full text-right md:duration-300 md:hover:scale-105 md:text-l lg:text-xl block pt-1 font-lato font-bold text-tempblack">About me</button>
                 </li>
                 <li>
-                  <a href="#" class="md:duration-300 md:hover:scale-105 shadow-xl p-1 px-5 rounded-xl items-center block bg-gradient-to-tr from-tempdarkblue to-templightblue">
+                  <button id="backToProjectsButton" class="md:duration-300 md:hover:scale-105 shadow-xl p-1 px-5 rounded-xl items-center block bg-gradient-to-tr from-tempdarkblue to-templightblue">
                     <div class="md:text-l lg:text-xl p-2 font-lato font-bold text-tempwhite">Projects</div>
-                  </a>
+                  </button>
                 </li>
                 <li>
-                  <a href="#" class="md:duration-300 md:hover:scale-105 shadow-xl p-1 px-3 lg:px-5 rounded-xl block bg-gradient-to-tr from-tempdarkgreen to-templightgreen">
+                  <button id="backToContactMeButton" class="md:duration-300 md:hover:scale-105 shadow-xl p-1 px-3 lg:px-5 rounded-xl block bg-gradient-to-tr from-tempdarkgreen to-templightgreen">
                     <div class="md:text-l lg:text-xl p-2 font-lato font-bold text-tempwhite">Contact Me</div>
-                  </a>
+                  </button>
                 </li>
               </ul>
              </div>
           </div>
         </nav>
     );
-}
-
-
+  }
+  
+  
 export default Navbar;
-
