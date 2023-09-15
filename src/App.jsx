13c5699db9ceb2b28734edf067fc28233c99bb75
2101dudu <eduardo21faria@gmail.com>
2023-09-15 19:30:20 +0100

    Finally added a back to top button that only appears on mobile devices. Changed the layout of the Projects section so that it becomes flex-column on larger devices, thus increasing visibility.

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,23 @@ import Footer from './components/Footer';
 
 function App() {
   useEffect(() => {
+    const backToTopButtonPhone = document.getElementById("backToTopButtonPhone");
     const backToTopButton = document.getElementById("backToTopButton");
     const backToAboutMeButton = document.getElementById("backToAboutMeButton");
     const backToProjectsButton = document.getElementById("backToProjectsButton");
     const backToContactMeButton = document.getElementById("backToContactMeButton");
 
+    window.onscroll = function () {
+      if ((document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) && window.screen.width <= 420) {
+          backToTopButtonPhone.classList.remove("hidden");
+      } else {
+          backToTopButtonPhone.classList.add("hidden");
+      }
+    }
+
+    const backToTopPhone = () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    };
     const backToTop = () => {
       window.scrollTo({ top: 0, behavior: "smooth" });
     };
@@ -27,6 +39,9 @@ function App() {
       window.scrollTo({ top: 2700, behavior: "smooth" });
     };
 
+    if (backToTopButtonPhone) {
+      backToTopButtonPhone.addEventListener("click", backToTopPhone);
+    }
     if (backToTopButton) {
       backToTopButton.addEventListener("click", backToTop);
     }
@@ -44,17 +59,21 @@ function App() {
   return (
     <>
       <div className="scroll-smooth">
+        <meta charset="utf-8"></meta>
         <header>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;500&family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet" />
         </header>
-        <Navbar />
-        <Hero />
-        <AboutMe />
-        <Projects />
-        <ContactMe />
-        <Footer />
+        <button id="backToTopButtonPhone" title="Back To Top" className="ease-in-out duration-300 hidden fixed z-10 bottom-8 right-8 border-0 w-16 h-16 rounded-full shadow-xl hover:scale-110 bg-gradient-to-tr from-tempdarkblue to-templightblue">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mx-auto text-tempwhite w-12"><path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75L12 3m0 0l3.75 3.75M12 3v18"/></svg>
+        </button>
+        <Navbar/>
+        <Hero/>
+        <AboutMe/>
+        <Projects/>
+        <ContactMe/>
+        <Footer/>
       </div>
     </>
   );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,40 +5,43 @@ function Projects () {
         <div class="px-8 pt-28 pb-40 md:py-40 bg-tempwhite">
           <div class="flex no-wrap flex-col items-center space-y-20 min-[470px]:space-y-28">
             <h1 class="text-tempdarkblue font-lato font-bold text-4xl sm:text-5xl md:text-6xl">Projects</h1>
-            <div class="flex flex-col min-[320px]:flex-row bg-tempwhite shadow-6xl m-auto">
-              <div class="w-full min-[320px]:w-1/2 md:w-80 lg:w-96">
+            <div class="flex flex-col min-[420px]:flex-row bg-tempwhite shadow-6xl m-auto">
+              <div class="w-full min-[420px]:w-1/2 md:w-80 lg:w-96">
                 <img class="w-full h-auto" src="src/assets/Wbsite_screenshot.png" alt="Website Screenshot"></img>
               </div>
-              <div class="w-full min-[320px]:w-1/2 md:w-80 lg:w-96 flex flex-col space-y-4 min-[320px]:space-y-0 justify-around items-center">
-                <h2 class="font-lato font-bold text-tempblack text-lg min-[320px]:text-sm min-[380px]:text-base min-[470px]:text-xl sm:text-2xl lg:text-3xl pt-2 min-[470px]:pt-3 sm:pt-5 md:pt-6">Portfolio Website</h2>
-                <a href="https://github.com/2101dudu/PortfolioWebsite" target="_blank" class="duration-300 hover:scale-105 min-[540px]:mt-4 bg-gradient-to-tr block from-tempdarkgreen to-templightgreen rounded-2xl py-1.5 px-4 min-[320px]:py-1 min-[320px]:px-3 min-[380px]:py-1.5 min-[380px]:px-4 md:py-2 md:px-4">
-                  <h3 class="text-tempwhite font-lato font-bold text-sm min-[320px]:text-[10px] min-[470px]:text-sm sm:text-base md:text-lg">View Project</h3>
+              <div class="w-full min-[420px]:w-1/2 md:w-80 lg:w-96 flex flex-col space-y-4 min-[320px]:space-y-6 min-[420px]:space-y-0 justify-around items-center">
+                <h2 class="font-lato font-bold text-tempblack text-lg min-[320px]:text-xl min-[420px]:text-base min-[470px]:text-xl sm:text-2xl lg:text-3xl pt-2 min-[470px]:pt-3 sm:pt-5 md:pt-6">Portfolio Website</h2>
+                <a href="https://github.com/2101dudu/PortfolioWebsite" target="_blank" class="duration-300 hover:scale-105 min-[540px]:mt-4 bg-gradient-to-tr block from-tempdarkgreen to-templightgreen rounded-2xl py-1.5 px-4 min-[320px]:py-2">
+                  <h3 class="text-tempwhite font-lato font-bold text-sm sm:text-base md:text-lg">View Project</h3>
                 </a>
-                <div class="flex flex-row space-x-2 min-[470px]:space-x-0 min-[470px]:flex-col pb-1 min-[470px]:pb-2 lg:pb-4 text-center">
-                  <h3 class="font-lato text-[10px] min-[320px]:text-[8px] mt-0.5 min-[380px]:mt-0 min-[380px]:text-[10px] sm:text-sm md:text-md text-tempblack">Built with</h3>
-                  <div class="flex space-x-1 min-[470px]:space-x-4">
-                      <img class="w-4 min-[470px]:w-5 min-[490px]:w-6 sm:w-8" src="src/assets/React_Logo.png" alt="React Logo"></img>
-                      <img class="w-4 min-[470px]:w-5 min-[490px]:w-6 sm:w-8" src="src/assets/Figma_Logo.png" alt="Figma Logo"></img>
+                <div class="flex flex-row space-x-2 min-[470px]:space-x-0 min-[470px]:flex-col pb-2 lg:pb-4 text-center">
+                  <h3 class="font-lato mt-0.5 min-[420px]:mt-0 text-[10px] min-[320px]:text-sm min-[420px]:text-[10px] sm:text-sm md:text-md text-tempblack">Built with</h3>
+                  <div class="flex space-x-2 min-[470px]:space-x-4">
+                      <img class="w-6 min-[420px]:w-5 min-[490px]:w-6 sm:w-8" src="src/assets/React_Logo.png" alt="React Logo"></img>
+                      <img class="w-6 min-[420px]:w-5 min-[490px]:w-6 sm:w-8" src="src/assets/Figma_Logo.png" alt="Figma Logo"></img>
                   </div>
                 </div>  
               </div>
             </div>
-            <div class="flex flex-col min-[320px]:flex-row bg-tempwhite shadow-6xl m-auto">
-              <div class="w-full min-[320px]:w-1/2 md:w-80 lg:w-96">
+            <div class="flex flex-col min-[420px]:flex-row bg-tempwhite shadow-6xl m-auto">
+              <div class="visible h-auto min-[420px]:invisible min-[420px]:h-0 min-[420px]:w-0 w-full">
                 <img class="w-full h-auto" src="src/assets/Wbsite_screenshot.png" alt="Website Screenshot"></img>
               </div>
-              <div class="w-full min-[320px]:w-1/2 md:w-80 lg:w-96 flex flex-col space-y-4 min-[320px]:space-y-0 justify-around items-center">
-                <h2 class="font-lato font-bold text-tempblack text-lg min-[320px]:text-sm min-[380px]:text-base min-[470px]:text-xl sm:text-2xl lg:text-3xl pt-2 min-[470px]:pt-3 sm:pt-5 md:pt-6">Portfolio Website</h2>
-                <a href="https://github.com/2101dudu/PortfolioWebsite" target="_blank" class="duration-300 hover:scale-105 min-[540px]:mt-4 bg-gradient-to-tr block from-tempdarkgreen to-templightgreen rounded-2xl py-1.5 px-4 min-[320px]:py-1 min-[320px]:px-3 min-[380px]:py-1.5 min-[380px]:px-4 md:py-2 md:px-4">
-                  <h3 class="text-tempwhite font-lato font-bold text-sm min-[320px]:text-[10px] min-[470px]:text-sm sm:text-base md:text-lg">View Project</h3>
+              <div class="w-full min-[420px]:w-1/2 md:w-80 lg:w-96 flex flex-col space-y-4 min-[320px]:space-y-6 min-[420px]:space-y-0 justify-around items-center">
+                <h2 class="font-lato font-bold text-tempblack text-lg min-[320px]:text-xl min-[420px]:text-base min-[470px]:text-xl sm:text-2xl lg:text-3xl pt-2 min-[470px]:pt-3 sm:pt-5 md:pt-6">Portfolio Website</h2>
+                <a href="https://github.com/2101dudu/PortfolioWebsite" target="_blank" class="duration-300 hover:scale-105 min-[540px]:mt-4 bg-gradient-to-tr block from-tempdarkgreen to-templightgreen rounded-2xl py-1.5 px-4 min-[320px]:py-2">
+                  <h3 class="text-tempwhite font-lato font-bold text-sm sm:text-base md:text-lg">View Project</h3>
                 </a>
-                <div class="flex flex-row space-x-2 min-[470px]:space-x-0 min-[470px]:flex-col pb-1 min-[470px]:pb-2 lg:pb-4 text-center">
-                  <h3 class="font-lato text-[10px] min-[320px]:text-[8px] mt-0.5 min-[380px]:mt-0 min-[380px]:text-[10px] sm:text-sm md:text-md text-tempblack">Built with</h3>
-                  <div class="flex space-x-1 min-[470px]:space-x-4">
-                      <img class="w-4 min-[470px]:w-5 min-[490px]:w-6 sm:w-8" src="src/assets/React_Logo.png" alt="React Logo"></img>
-                      <img class="w-4 min-[470px]:w-5 min-[490px]:w-6 sm:w-8" src="src/assets/Figma_Logo.png" alt="Figma Logo"></img>
+                <div class="flex flex-row space-x-2 min-[470px]:space-x-0 min-[470px]:flex-col pb-2 lg:pb-4 text-center">
+                  <h3 class="font-lato mt-0.5 min-[420px]:mt-0 text-[10px] min-[320px]:text-sm min-[420px]:text-[10px] sm:text-sm md:text-md text-tempblack">Built with</h3>
+                  <div class="flex space-x-2 min-[470px]:space-x-4">
+                      <img class="w-6 min-[420px]:w-5 min-[490px]:w-6 sm:w-8" src="src/assets/React_Logo.png" alt="React Logo"></img>
+                      <img class="w-6 min-[420px]:w-5 min-[490px]:w-6 sm:w-8" src="src/assets/Figma_Logo.png" alt="Figma Logo"></img>
                   </div>
                 </div>  
+              </div>
+              <div class="invisible h-0 min-[420px]:visible min-[420px]:h-auto w-full min-[420px]:w-1/2 md:w-80 lg:w-96">
+                <img class="w-full h-auto" src="src/assets/Wbsite_screenshot.png" alt="Website Screenshot"></img>
               </div>
             </div>
           </div>
